f8bea1badaf48b0a29e0e203131c61b39ea00639
Nawit-dev <feo.nickita@yandex.ru>
2023-05-07 10:29:56 +0300

    creature TODOLIST

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const TodoList = () => {
+    return <ul >
+        <li > Learn React </li> 
+        <li > build Awesome App </li> 
+        </ul>
+}
+const AppHeader =()=>{
+    return <h1 > My Todo list </h1> 
+}
+
+const SearchPanel=()=> {
+return <input placeholder='seasrch'/>
+}
+
+const App =()=>{
+    return(   <div >
+        <AppHeader/>
+        <SearchPanel/>
+       <TodoList/>
+       </div >)
+} 
+
+const el = ( 
+   <div >
+     <AppHeader/>
+     <SearchPanel/>
+    <TodoList/>
+    </div >
+
+)
+ReactDOM.render(<App/>, document.getElementById('root'));
